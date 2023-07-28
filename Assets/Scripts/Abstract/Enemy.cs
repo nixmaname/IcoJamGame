f89b56e1d94ddb11d05f63248ca15992c6aab2e8
Stanislav <taix97@outlook.com>
2023-07-28 20:51:44 +0300

    different enemies

--- Assets/Scripts/Abstract/Enemy.cs
+++ Assets/Scripts/Abstract/Enemy.cs
@@ -4,8 +4,28 @@ public abstract class Enemy : MonoBehaviour
 {
     public int speed;
 
-    public GameObject waypointA, waypointB;
+    public bool vertical;
 
-    public abstract void Moving();
+    private bool isFacingRight;
+
+    public virtual void Moving()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.Self);
+    }
+
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        if (isFacingRight)
+        {
+            transform.eulerAngles = !vertical ? new Vector3(0f, -180f, 0f) : new Vector3(-180f, 0f, 90f);
+        }
+        else
+        {
+            transform.eulerAngles = !vertical ? Vector3.zero : new Vector3(0f, 0f, 90f);
+        }
+    }
+
 
 }
