2fbdf66d2fa9d73ce5714c550454fbd8afd8f1fc
mnnice <mcmcnaix@live.com>
2023-07-31 12:05:40 +0300

    levels

--- Assets/Audio/MusicChoice.cs
+++ Assets/Audio/MusicChoice.cs
@@ -6,10 +6,21 @@ public class MusicChoice : MonoBehaviour
 {
     public AudioClip hotAndCold, random;
     AudioSource aud;
+    int nl;
     // Start is called before the first frame update
     void Start()
     {
         aud = GetComponent<AudioSource>();
+        nl = GameObject.FindGameObjectWithTag("EndLevel").transform.GetComponent<NextLevel>().nextLevel-1;
+        if(nl%2 == 0)
+        {
+            aud.clip = hotAndCold;
+        }
+        else
+        {
+            aud.clip = random;
+        }
+        aud.Play();
     }
 
     // Update is called once per frame
